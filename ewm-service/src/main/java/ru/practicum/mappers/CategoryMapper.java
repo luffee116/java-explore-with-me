4fbff62f8747b83a67f7f234925cdbbdf2f72b88
Ruslan Aliullov <aliullov2016@mail.ru>
2025-08-11 22:31:31 +0300

    Checkstyle

--- ewm-service/src/main/java/ru/practicum/mappers/CategoryMapper.java
+++ ewm-service/src/main/java/ru/practicum/mappers/CategoryMapper.java
@@ -4,7 +4,7 @@ import ru.practicum.dto.category.CategoryDto;
 import ru.practicum.entity.category.Category;
 
 public class CategoryMapper {
-    public static CategoryDto toDto(Category category){
+    public static CategoryDto toDto(Category category) {
         return new CategoryDto(category.getId(), category.getName());
     }
 }
